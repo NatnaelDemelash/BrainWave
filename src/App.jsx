d5099eb0d07420e37a3247167a8cc34416a293d7
NatnaelDemelash <natidemelash07@gmail.com>
2024-10-02 12:01:40 +0300

    Created a resusable button/Link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Button from "./components/Button";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
 const App = () => {
   return (
-    <div>
+    <>
       <h1>Hello World</h1>
-    </div>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
   );
 };
 
